0bfba05dd6c957a83cd7653cb4310a8b7b6095ec
angelvalbuena <angeljv_241@hotmail.com>
2017-01-14 12:22:22 -0500

    url container

--- src/test/java/restTest/ressTest/ConsultParTest.java
+++ src/test/java/restTest/ressTest/ConsultParTest.java
@@ -19,7 +19,7 @@ import static com.jayway.restassured.RestAssured.given;
 
 public class ConsultParTest {
 	
-	String baseURL = "http://172.17.0.2:8080";
+	String baseURL = "http://172.17.0.4:8080";
 	String path = "/cloudBRT/api/monitoreo/paradas/consultar/ST1";
 	
 	@Test
